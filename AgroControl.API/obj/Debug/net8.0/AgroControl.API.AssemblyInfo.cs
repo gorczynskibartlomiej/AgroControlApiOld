ce5dc6dcd3d81abcba44a7b6881908af6c4bf957
Bartłomiej Górczyński <gorczynskibartlomiej07@gmail.com>
2024-10-24 09:43:19 +0200

    UI moved to other repository

--- AgroControl.API/obj/Debug/net8.0/AgroControl.API.AssemblyInfo.cs
+++ AgroControl.API/obj/Debug/net8.0/AgroControl.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgroControl.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2dea5d44174ede77c590ee0f531695416ed8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c692b9a458b87af53a29a8acccb1a6486eebdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgroControl.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgroControl.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
